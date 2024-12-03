0c668fbdcc594df27f9d102f5da403d70bc9cd91
diptadeepray <diptadeepray@gmail.com>
2024-12-04 01:55:28 +0530

    Main_Activity_And_Form_UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.paceapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.paceapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

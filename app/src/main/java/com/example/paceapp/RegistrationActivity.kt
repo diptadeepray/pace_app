779f47dd37aa43bfc684011484eef124e2d7c603
diptadeepray <diptadeepray@gmail.com>
2024-12-12 15:12:51 +0530

    LoginNavigation_GoingOn

---
+++ app/src/main/java/com/example/paceapp/RegistrationActivity.kt
@@ -0,0 +1,51 @@
+package com.example.paceapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.view.View
+
+import android.content.Intent
+
+//This import is used, so that we can use ViewBinding
+//i.e. use objects of the layouts instead of findViewById
+import com.example.paceapp.databinding.LoginActivityBinding
+
+//Importing this will not do the job
+import androidx.fragment.app.Fragment
+
+//We need to import the following
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+import android.widget.Button
+import android.widget.TextView
+import com.example.paceapp.databinding.RegistrationActivityBinding
+
+
+class RegistrationActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: RegistrationActivityBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+
+        
+        binding = RegistrationActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registrationBtnBack.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+    }}
+
+
+
---
+++ app/src/main/java/com/example/paceapp/ResetPasswordActivity.kt
@@ -0,0 +1,46 @@
+package com.example.paceapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.view.View
+
+import android.content.Intent
+
+//This import is used, so that we can use ViewBinding
+//i.e. use objects of the layouts instead of findViewById
+import com.example.paceapp.databinding.LoginActivityBinding
+
+//Importing this will not do the job
+import androidx.fragment.app.Fragment
+
+//We need to import the following
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+import android.widget.Button
+import android.widget.TextView
+import com.example.paceapp.databinding.ResetPasswordActivityBinding
+
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ResetPasswordActivityBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //enableEdgeToEdge()
+
+
+        binding = ResetPasswordActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+
+
+    }}
